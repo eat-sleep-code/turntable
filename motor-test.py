c4189b151e7505c9a595d5a5c752891399d5dcdc
eat-sleep-code <info@eat-sleep-code.com>
2021-05-23 08:07:35 -0700

    Fix extraneous from statement

--- motor-test.py
+++ motor-test.py
@@ -2,11 +2,10 @@ import time
 import board
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
-from 
 
 motors = MotorKit()
 
-for i in range(36800):
+for i in range(36800): # ~ 100 turns 
     motors.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
     print(str(i))
     time.sleep(0.01)
